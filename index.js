8def9aaa3fc0368194cd7c73159c552f7561634b
ashish <87756049+asish-op@users.noreply.github.com>
2021-11-02 15:42:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const {mineboty} = require("mineboty")
 mineboty();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World! bot is in game and working if it is not working pls join discord and ask for help team-IC discord link : https://discord.gg/5AuwhVycjv'));
+
+console.log(`this is made by team ic`);
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
+//remove this if you are not using replit 
+//const express = require('express');
+//const app = express();
+//const port = 3000;
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
