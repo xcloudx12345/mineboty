baeafa2524c530019aca05ddca7f937cab761ddd
ashish <87756049+asish-op@users.noreply.github.com>
2021-11-06 08:37:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World! bot is in game and working if it is not working pls join discord and ask for help team-IC discord link : https://discord.gg/5AuwhVycjv'));
 
 console.log(`this is made by team ic`);
+console.log(`star us in github`);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
@@ -17,7 +18,7 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 //const app = express();
 //const port = 3000;
 
-//app.get('/', (req, res) => res.send('Hello World!'));
+//app.get('/', (req, res) => res.send('Hello World! bot is in game and working if it is not working pls join discord and ask for help team-IC discord link : https://discord.gg/5AuwhVycjv'));
 
 //app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
