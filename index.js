c170963df7144d13ba36f9dfcf63cd8e16ba499f
ashish <87756049+asish-op@users.noreply.github.com>
2021-11-25 13:14:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World! bot is in game and working if it is not working pls join discord and ask for help team-IC discord link : https://discord.gg/5AuwhVycjv'));
 
-console.log(`this is made by team ic`);
-console.log(`star us in github`);
+console.log(`
+███████╗
+██╔════╝
+█████╗░░
+██╔══╝░░
+███████╗
+╚══════╝`);
+
+
+
+console.log(`
+████████╗███████╗░█████╗░███╗░░░███╗░░░░░░██╗░█████╗░
+╚══██╔══╝██╔════╝██╔══██╗████╗░████║░░░░░░██║██╔══██╗
+░░░██║░░░█████╗░░███████║██╔████╔██║█████╗██║██║░░╚═╝
+░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║╚════╝██║██║░░██╗
+░░░██║░░░███████╗██║░░██║██║░╚═╝░██║░░░░░░██║╚█████╔╝
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░░╚═╝░╚════╝░ \n \n \n \nmineboty developers \n *Kartik OP*#4791 \n youtube = "https://www.youtube.com/c/CodingIndia" \n \n \n ||**AshishOp.java**||#9999 \n youtube = "https://youtube.com/c/gtasish" \n \n \n \n \n \n website = "https://teamic.ml" \n \n  `);
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)); */
 
