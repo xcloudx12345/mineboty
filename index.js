983d22bf6032b23637d95b77df012c7b957cda54
ashish <87756049+asish-op@users.noreply.github.com>
2021-11-25 13:16:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,13 +32,4 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 
 
 
-//remove this if you are not using replit 
-//const express = require('express');
-//const app = express();
-//const port = 3000;
-
-//app.get('/', (req, res) => res.send('Hello World! bot is in game and working if it is not working pls join discord and ask for help team-IC discord link : https://discord.gg/5AuwhVycjv'));
-
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
 
